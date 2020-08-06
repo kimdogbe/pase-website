3b19763eb26cdb25da8f9a2428a75c27fa62d60d
kimdogbe <kimdogbe@gmail.com>
2020-08-06 14:30:24 +0100

    added static json

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,146 +1,75 @@
 <template>
-  <v-container>
+  <v-container mb-6>
     <v-row>
-      <v-col cols="4">
-        <v-card>
-          <v-img
-              src="@/assets/farm-workers.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-          >
-            <v-row
-              align="center"
-              justify="center"
-            >
-              <v-col class="text-center" cols="12">
-                <h1>Our Mission</h1>
-              </v-col>
-            </v-row>
-          </v-img>
-        </v-card>
-      </v-col>
+      <v-dialog 
+      v-for="topic in topics" 
+      :key="topic.name"
+      v-model="topic.dialog" 
+      width="700">
+        <template v-slot:activator="{ on, attrs}">
+          <v-col cols="4">
+            <v-card link v-bind="attrs" v-on="on">
+              <v-img
+                  :src="topic.image"
+                  class="white--text align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="200px"
+              >
+                <v-row
+                  align="center"
+                  justify="center"
+                >
+                  <v-col class="text-center" cols="12">
+                    <h1>{{ topic.name }}</h1>
+                  </v-col>
+                </v-row>
+              </v-img>
+            </v-card>
+          </v-col>
+        </template>
 
-      <v-col cols="4">
         <v-card>
-          <v-img
-              src="@/assets/mill-workers.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-          >
-            <v-row
-              align="center"
-              justify="center"
-            >
-              <v-col class="text-center" cols="12">
-                <h1>Our Vision</h1>
-              </v-col>
-            </v-row>
-          </v-img>
-        </v-card>
-      </v-col>
+          <v-card-title class="headline">
+            {{ topic.name }}
+          </v-card-title>
 
-      <v-col cols="4">
-        <v-card>
-          <v-img
-              src="@/assets/weighing-ffb.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-          >
-            <v-row
-              align="center"
-              justify="center"
-            >
-              <v-col class="text-center" cols="12">
-                <h1>Processes</h1>
-              </v-col>
-            </v-row>
-          </v-img>
-        </v-card>
-      </v-col>
+          <v-card-text>
+            {{ topic.text }}
+          </v-card-text>
 
-      <v-col cols="4">
-        <v-card>
-          <v-img
-              src="@/assets/team.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-          >
-            <v-row
-              align="center"
-              justify="center"
-            >
-              <v-col class="text-center" cols="12">
-                <h1>Organisation and Board</h1>
-              </v-col>
-            </v-row>
-          </v-img>
-        </v-card>
-      </v-col>
+          <v-divider></v-divider>
 
-      <v-col cols="4">
-        <v-card>
-          <v-img
-              src="@/assets/mill.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-          >
-            <v-row
-              align="center"
-              justify="center"
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click="topic.dialog = false"
             >
-              <v-col class="text-center" cols="12">
-                <h1>Mill</h1>
-              </v-col>
-            </v-row>
-          </v-img>
+              Done
+            </v-btn>
+          </v-card-actions>
         </v-card>
-      </v-col>
-
-      <v-col cols="4">
-        <v-card>
-          <v-img
-              src="@/assets/cracked-palm.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-          >
-            <v-row
-              align="center"
-              justify="center"
-            >
-              <v-col class="text-center" cols="12">
-                <h1>Plans</h1>
-              </v-col>
-            </v-row>
-          </v-img>
-        </v-card>
-      </v-col>
-
-      <v-col cols="4">
-        <v-card>
-          <v-img
-              src="@/assets/threshing-ffb.jpg"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-          >
-            <v-row
-              align="center"
-              justify="center"
-            >
-              <v-col class="text-center" cols="12">
-                <h1>History</h1>
-              </v-col>
-            </v-row>
-          </v-img>
-        </v-card>
-      </v-col>
+      </v-dialog>
 
     </v-row>
   </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        topics: [
+          {name: "Our Vision", image: require("@/assets/farm-workers.jpg"), text: "Text about the Vision", dialog: false},
+          {name: "Our Mission", image: require("@/assets/mill-workers.jpg"), text: "Like the Songhai Centre in the Republic of Benin, from which PASE was inspired, PASE seeks to promote dynamic integrated human development of women and young farmers through the use of local resources. This will be done by simultaneously spurring agricultural production, processing, mechanisation and extension services, and building on indigenous knowledge. This will create ownership of the development process, and of the technical and external technologies, among the people and communities in which PASE operates. PASE will also seek to inspire community-spirited individuals to venture into social enterprises as a sustainable way of empowering the poor and marginalised.", dialog: false},
+          {name: "Processes", image: require("@/assets/weighing-ffb.jpg"), text: "Text about the processes performed", dialog: false},
+          {name: "Organisation and Board", image: require("@/assets/team.jpg"), text: "Text about the organisation and the board", dialog: false},
+          {name: "Mill", image: require("@/assets/mill.jpg"), text: "Text describing the mill", dialog: false},
+          {name: "Plans", image: require("@/assets/cracked-palm.jpg"), text: "Text describing furture plans", dialog: false},
+          {name: "History", image: require("@/assets/threshing-ffb.jpg"), text: "Text describing the history", dialog: false},
+        ],
+      }
+    },
+  }
+</script>
